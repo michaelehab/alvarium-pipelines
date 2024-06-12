cea54cd9b9bc3e07c7c488ea0e44771c7ee90ca2
Michael Ehab <michael.ehab@hotmail.com>
2024-06-12 13:33:13 +0300

    Use the exported source code checksum calculation code

--- vars/getSourceCodeChecksum.groovy
+++ vars/getSourceCodeChecksum.groovy
@@ -1,92 +1,11 @@
-import java.security.MessageDigest
-import java.nio.file.Files
-import java.nio.file.Paths
+@GrabResolver(name='jitpack.io', root='https://jitpack.io/')
+@Grab("com.google.errorprone:error_prone_annotations:2.20.0") // fixes alvarium import error
+@Grab(group='com.github.michaelehab', module='alvarium-sdk-java', version='c981582bf6') 
+@Grab("org.apache.logging.log4j:log4j-core:2.23.1")
 
-class HashProvider {
-    MessageDigest md
-
-    HashProvider() {
-        md = MessageDigest.getInstance("MD5")
-    }
-
-    @NonCPS
-    void update(byte[] buffer, int offset, int length) {
-        md.update(buffer, offset, length)
-    }
-
-    @NonCPS
-    void update(byte[] buffer) {
-        md.update(buffer)
-    }
-
-    @NonCPS
-    String getValue() {
-        byte[] digest = md.digest()
-        digest.collect { String.format("%02x", it) }.join()
-    }
-
-    @NonCPS
-    String derive(byte[] input) {
-        md.update(input)
-        getValue()
-    }
-}
-
-@NonCPS
-def getAllFiles(String path) {
-    def files = []
-    def directory = new File(path)
-
-    if (directory.isDirectory()) {
-        def directoryFiles = directory.listFiles()
-        if (directoryFiles != null) {
-            directoryFiles.each { file ->
-                if (file.isFile()) {
-                    files << file.getAbsolutePath()
-                } else if (file.isDirectory()) {
-                    files += getAllFiles(file.getAbsolutePath())
-                }
-            }
-        }
-    } else if (directory.isFile()) {
-        files << directory.getAbsolutePath()
-    }
-    files
-}
-
-@NonCPS
-def readAndHashFile(String filePath, HashProvider hashProvider) {
-    def fs = new FileInputStream(filePath)
-    def buffer = new byte[8192]
-    def bytesRead = 0
-    while (true) {
-        bytesRead = fs.read(buffer)
-        if (bytesRead == -1) { // indicates EOF
-            break
-        } else {
-            hashProvider.update(buffer, 0, bytesRead)
-        }
-    }
-    fs.close()
-    hashProvider.getValue()
-}
-
-@NonCPS
-def generateChecksum(String path, HashProvider hashProvider) {
-    def filePaths = getAllFiles(path)
-    for (int i = 0; i < filePaths.size(); i++) {
-        def hashThenPath = readAndHashFile(filePaths[i], hashProvider) + "  " + filePaths[i]
-        filePaths[i] = hashThenPath
-    }
-    filePaths.sort()
-
-    def hashesAndFiles = filePaths.join("\n") + "\n"
-    def sourceCodeChecksum = hashProvider.derive(hashesAndFiles.getBytes())
-
-    sourceCodeChecksum
-}
+import com.alvarium.annotators.sourcecode.CheckSumCalculator;
 
 def call(sourceCodeDir) {
-    def hashProvider = new HashProvider()
-    generateChecksum(sourceCodeDir, hashProvider)
+    CheckSumCalculator checkSumCalculator = new CheckSumCalculator("md5")
+    checkSumCalculator.generateChecksum(sourceCodeDir)
 }
