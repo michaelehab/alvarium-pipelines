f1ce25d8483b400fdad167f869f9574cd1c64695
Michael Ehab <michael.ehab@hotmail.com>
2024-06-24 12:08:34 +0300

    Update used java sdk version

--- vars/alvariumGetAnnotators.groovy
+++ vars/alvariumGetAnnotators.groovy
@@ -1,6 +1,6 @@
 @GrabResolver(name='jitpack.io', root='https://jitpack.io/')
 @Grab("com.google.errorprone:error_prone_annotations:2.20.0") // fixes alvarium import error
-@Grab(group='com.github.michaelehab', module='alvarium-sdk-java', version='c981582bf6') 
+@Grab(group='com.github.michaelehab', module='alvarium-sdk-java', version='d6abe902dc') 
 @Grab("org.apache.logging.log4j:log4j-core:2.23.1")
 
 import java.util.Map;
